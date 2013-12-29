5360b6bd099aae7fe4856a677ec581ea447800b0
James Storey <storey.james@gmail.com>
2013-12-29 17:19:46 -0600

    finished EffectComposer current feature list

--- EffectComposer.js
+++ EffectComposer.js
@@ -11,9 +11,9 @@
 // 3 render to that target with the main camera and scene
 // 4 call EffectComposer.render to render to the screen with the effects applied
 
-var EffectComposer = function (renderer, renderTarget) {
-	this.renderer = renderer;
-
+var EffectComposer = function (r, rt) {
+	var renderer = r;
+	var renderTarget = rt;
 	if (renderTarget === undefined) {
 		var width = window.innerWidth || 1;
 		var height = window.innerHeight || 1;
@@ -30,31 +30,31 @@ var EffectComposer = function (renderer, renderTarget) {
 	var writeBuffer = renderTarget1;
 	var readBuffer = renderTarget2;
 
-	this.passes = [];
-	this.rawRenders = [];
-	this.combineIndex = false;
+	var passes = [];
+	var rawRenders = [];
+	var combineIndex = -1;
 
 	var camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 
 	var scene = new THREE.Scene();
 	
 	var mesh = new THREE.Mesh(new THREE.PlanarGeometry(2,2), null);
-	scene.add(this.mesh);
+	scene.add(mesh);
 
 	var passVertex = """
-
+	varying vec2 vUv;
 	void main() {
-		gl_Position = vec4(position, 1.0);
+		vUv = uv;
+		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 	}
 	""";
 
 	var blitShader = """
-	uniform vec2 resolution;
-	uniform sampler2D texture; 
+	uniform sampler2D texture;
+	varying vec2 vUv;
 	void main() {
-		vec2 uv = gl_FragCoord.xy / resolution.xy;
-		vec3 color = texture2D(texture, uv).xyz;
-		gl_FragColor = vec4(color, 1.0);
+		vec4 texel = texture2D(texture, vUv);
+		gl_FragColor = texel;
 	}
 	""";
 
@@ -65,7 +65,6 @@ var EffectComposer = function (renderer, renderTarget) {
 	}
 
 	var passThruUniforms = {
-		resolution: {type: "v2", value: res },
 		texture: { type: "t", value: null }
 	}
 
@@ -75,43 +74,55 @@ var EffectComposer = function (renderer, renderTarget) {
 		fragmentShader: blitShader
 	});
 
-	this.renderToScreen = function (inTexture) {
+	var that = {};
+
+	var renderToScreen = function (inTexture) {
+		// render the result to the screen
+		// serves as final output
 		if(inTexture === undefined) {
 			passThruUniforms.texture = readBuffer;	
 		}
 		else {
 			passThruUniforms.texture = inTexture;
 		}
-		this.mesh.material = passThruShader;
-		renderer.render(this.scene, this.camera);
+		mesh.material = passThruShader;
+		renderer.render(scene, camera);
 	}
 
-	this.renderPass = function(passNum) {
+	var renderPass = function(passNum) {
+		// render a single pass as found in the passes array
 		var currentPass = passes[passNum];
 		if(currentPass.uniforms.texture !== undefined) {
-			currentPass.uniforms.texture = this.readBuffer;
+			currentPass.uniforms.texture = readBuffer;
+		}
+		else if(currentPass.uniforms.tDiffuse !== undefined) {
+			// compatibility with alteredq style shader names
+			currentPass.uniforms.tDiffuse = readBuffer;
 		}
 
 		mesh.material = currentPass;
-		renderer.render(this.scene, this.camera, this.writeBuffer);
+		renderer.render(scene, camera, writeBuffer);
 
-		this.swapBuffers();
+		swapBuffers();
 	}
 
 	var renderPassThru = function(input, output) {
+		// renders a texture to another target;
 		mesh.material = passThruShader;
 		passThruShader.uniforms.texture = input;
-		renderer.render(this.scene, this.camera, output);
-		this.swapBuffers();
+		renderer.render(scene, camera, output);
+		swapBuffers();
 	}
 
-	this.render = function() {
-
-		for(var j = 0; j < this.rawRenders.length; j += 1) {
-			renderPassThru(rawRenders[j] ,this.writeBuffer);
+	var render = function() {
+		// renders all passes to each rendertarget entry up to the combine pass
+		for(var j = 0; j < rawRenders.length; j += 1) {
+			renderPassThru(rawRenders[j], writeBuffer);
 			for(var i = 0; i < passes.length; i += 1) {
-				if(this.combineIndex !== false && i === this.combineIndex) {
-					renderPassThru(this.readBuffer, rawRenders[j]);
+				if(combineIndex > 0 && i === combineIndex) {
+					// put the rendertargets back onto the input buffers
+					// this is for access by the combine shader
+					renderPassThru(readBuffer, rawRenders[j]);
 					break;
 				}
 				renderPass(i);
@@ -119,37 +130,55 @@ var EffectComposer = function (renderer, renderTarget) {
 			}
 		}
 
-		if(this.combineIndex !== false ) {
-			var cShader = passes[this.combineIndex];
-			cShader.uniforms.texture1 = rawRenders[0];
-			cShader.uniforms.texture2 = rawRenders[1];
+		// render the combine step following that shader's routines
+		if(combineIndex > 0) {
+			var cShader = passes[combineIndex];
+			try {
+				cShader.uniforms.texture1 = rawRenders[0];
+				cShader.uniforms.texture2 = rawRenders[1];
+			}
+			catch (e) {
+				console.error("combine shader needs texture targets 'texture1' and 'texture2'");
+			}
 
-			renderPass(this.combineIndex);
+			renderPass(combineIndex);
 		}
 
+		// render the rest of the steps on the combination buffer
 		for(var i = this.combineIndex + 1; i < passes.length; i += 1) {
 			renderPass(i);
 		}
 
-		this.renderToScreen();
+		renderToScreen();
 	}
 
-	this.getRenderTarget = function () {
+	var getRenderTarget = function () {
+		// returns a new input to render a scene on to.
+		// this serves as the entry point to the effectComposer
+		// if more than one renderTarget is called, effects up to a combination step are run in parallel
 		var rt = renderTarget.clone();
-		this.rawRenders.push(rt);
+		rawRenders.push(rt);
 		return rt;
 	}
 
-	this.addShaderPass = function (obj, combine) {
+	var addShaderPass = function (obj, combine) {
+		// add a regular screen space shader.
+		// will expect at least one texture input named 'texture'
 		if(obj instanceof THREE.ShaderMaterial) {
-			this.passes.push(obj);
+			passes.push(obj);
 			if(combine === true) {
-
+				if(combineIndex > 0) {
+					console.error("too many combine steps in EffectComposer");
+				}
+				else {
+					combineIndex = passes.length - 1;
+				}
 			}
 		}
 	}
 
-	this.reset = function (renderTarget) {
+	var reset = function (renderTarget) {
+		//reinitialize the render targets to a new size;
 		if(renderTerget === undefined) {
 			var width = window.innerWidth || 1;
 			var height = window.innerHeight || 1;
@@ -160,18 +189,28 @@ var EffectComposer = function (renderer, renderTarget) {
 			renderTarget = new THREE.WebGLRenderTarget(width, height, parameters);
 		}
 
-		this.renderTarget1 = renderTarget;
-		this.renderTarget2 = renderTarget.clone();
+		renderTarget1 = renderTarget;
+		renderTarget2 = renderTarget.clone();
 
-		this.writeBuffer = renderTarget1;
-		this.readBuffer = renderTarget2;
+		writeBuffer = renderTarget1;
+		readBuffer = renderTarget2;
 	}
 
-	this.setSize = function (w, h) {
-		var rt = this.renderTarget1.clone();
+	var setSize = function (w, h) {
+		var rt = renderTarget1.clone();
 		rt.width = w;
 		rt.height = h;
 
-		this.reset(rt);
+		reset(rt);
 	}
+
+	that.setSize = setSize;
+	that.reset = reset;
+	that.render = render;
+	that.addShaderPass = addShaderPass;
+	that.getRenderTarget = getRenderTarget;
+	that.renderPassThru = renderPassThru;
+	that.renderPass = renderPass;
+
+	return that;
 }
