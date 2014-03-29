ac08e609da675cd989aed47f31859b22ab6da198
James Storey <storey.james@gmail.com>
2014-03-29 16:53:25 -0500

    working start on generic loading screen

--- loadScreen/LoadingScreen.js
+++ loadScreen/LoadingScreen.js
@@ -1,6 +1,5 @@
 
 // interface to load assets and provide progress reports to frontend
-
 var AssetFactory = function (onFinish, onProgress) {
 	var that = {};
 	var map = {};
@@ -39,6 +38,8 @@ var AssetFactory = function (onFinish, onProgress) {
 	var loadAssets = function (assetMap, callback, progressCallback) {
 		filesToLoad = Object.keys(assetMap).length;
 		filesLoaded = 0;
+		onProgress = progressCallback;
+		onFinish = callback;
 		for (var prop in assetMap) {
 			if(typeof assetMap[prop] === "string") {
 				var uri = assetMap[prop];
@@ -60,21 +61,94 @@ var AssetFactory = function (onFinish, onProgress) {
 		}
 	};
 
+	// for testing draw
+	var dummyLoad = function(callback, progressCallback) {
+		onProgress = progressCallback;
+		onFinish = callback;
+		var dummyTime = 0;
+		var timerInterval = setInterval(function() {
+			dummyTime += 1;
+			if(dummyTime < 10) { 
+				onProgress(dummyTime, 10);
+			}
+			else {
+				clearInterval(timerInterval);
+				onFinish();
+			}
+		}, 1000);
+	};
+
 	that.loadAssets = loadAssets;
+	that.dummyLoad = dummyLoad;
 	that.map = map;
 	return that;
 };
 
-var LoadingScreen = function (renderer, width, height, assetMap) {
+var LoadingScreen = function (width, height, assetMap, callback) {
 	var factory = new AssetFactory();
 	var map = assetMap || {};
 	var that = {};
-	var loadingScene = THREE.scene();
-	var camera = THREE.OrthographicCamera(  width / -2, width / 2,  height / 2,  height / -2, 10, 200);
+	var loading = false;
+	var done = false;
 
+	var barBorder = document.getElementById("barBorder");
+	var elemHeight = height / 10;
+	var elemWidth = width * 0.8;
+	barBorder.style.height = elemHeight.toString();
+	barBorder.style.width = elemWidth.toString();
+	barBorder.style.top = (height / 2 - elemHeight).toString();
+	barBorder.style.left = (elemWidth - width / 2).toString();
+
+	var loadingBar = document.getElementById("loadingBar");
+	loadingBar.style.height = elemHeight.toString();
 	
+	var finished = function() {
+		if (typeof callback === "function")
+			callback();
+		else
+			hideLoadingScreen();
+		loading = false;
+	};
 
-	var render = function() {
+	var progress = function(loaded, total) {
+		// update graphic
+		var pcent = (loaded / total) * 100;
+		loadingBar.style.width = pcent.toString() + "%";
+	};
+
+	// call from the main program's render loop
+	// first call starts the load requests
+	var animate = function() {
+		if(loading === false) {
+			loading = true;
+			if(typeof assetMap !== "object") {
+				//factory.loadAssets(assetMap, finished, progress);
+				factory.dummyLoad(finished, progress);
+			}
+			else {
+				factory.loadAssets(assetMap, finished, progress);
+			}
+		}
 
 	};
+
+	var hideLoadingScreen = function () {
+		barBorder.style.display = "none";
+		loadingBar.style.display = "none";
+	};
+
+	that.animate = animate;
+	that.done = done;
+
+	return that;
 };
+
+var lScreen = new LoadingScreen(640, 480);
+
+var testAnim = function() {
+	if(lScreen.done === false)
+		requestAnimationFrame(testAnim);
+	lScreen.animate();
+};
+
+testAnim();
