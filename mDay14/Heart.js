8565b9f36c62b8ced73d73db78c107d1aa18a954
James Storey <storey.james@gmail.com>
2014-05-10 23:50:37 -0500

    sorted heart by y

--- mDay14/Heart.js
+++ mDay14/Heart.js
@@ -2,10 +2,56 @@ var Heart = function(){
 	var that = {};
 	that.loaded = false;
     var heartMesh;
-    t = 0;
+    var ySort = [];
+    var t = 0;
 
    	var pos = {y:-150};
     var tween;
+
+    var insertVByY = function (a, v, i)
+    {
+        // base case
+        var pos = Math.floor(a.length / 2);
+        if(a.length === 0)
+        {
+            a.push(i);
+            return a;
+        }
+        else if (a.length === 1) {
+            if(v.y < heartMesh.geometry.vertices[a[pos]].y) {
+                var n = []
+                n.push(i);
+                return n.concat(a);
+            }
+            else {
+                a.push(i);
+                return a;
+            }
+        }
+
+        // split
+        var aG = a.slice(pos);
+        var aL = a.slice(0, pos);
+        
+        //sort logic
+        if(v.y > heartMesh.geometry.vertices[a[pos]].y) {
+            aG = insertVByY(aG, v, i);
+        }
+        else {
+            aL = insertVByY(aL, v, i);
+        }
+
+        // combine
+        return aL.concat(aG);
+    }
+
+    var sortVertsY = function () {
+        for (var i = 0; i < heartMesh.geometry.vertices.length; i += 1) {
+            var v = heartMesh.geometry.vertices[i];
+            var r = insertVByY(ySort, v, i);
+            ySort = r;
+        }
+    };
 
     var load = function () {
     	var loader = new THREE.JSONLoader();
@@ -16,6 +62,10 @@ var Heart = function(){
 		 	that.loaded = true;
 		 	console.log("heart loaded");
 		 	scene.add(heartMesh);
+            sortVertsY();
+            //heartMesh.geometry.vertices[ySort[0]].setY(0);
+            //heartMesh.geometry.vertices[ySort[ySort.length - 1]].setY(0);
+            //heartMesh.geometry.verticesNeedUpdate = true;
 	 	});
     };
 
